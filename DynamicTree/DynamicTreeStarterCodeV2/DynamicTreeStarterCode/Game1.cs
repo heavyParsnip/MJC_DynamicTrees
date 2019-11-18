4fa43dde70cb452bcbe80bb0fb37ecf8bce9ab5d
Matthew Camera <mjc9536@rit.edu>
2019-11-18 13:19:26 -0500

    PE Completed

---
+++ DynamicTree/DynamicTreeStarterCodeV2/DynamicTreeStarterCode/Game1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DynamicTreeStarterCode
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game
+	{
+        Random rand;
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+		// The three trees
+		Tree treeRed;
+		Tree treeGreen;
+		Tree treeBlue;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1000;
+            graphics.ApplyChanges();
+            Content.RootDirectory = "Content";
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			// Create the three trees
+			treeRed = new Tree(spriteBatch, Color.Red);
+			treeGreen = new Tree(spriteBatch, Color.Green);
+			treeBlue = new Tree(spriteBatch, Color.DodgerBlue);
+
+            // *** FILL EACH TREE WITH DATA HERE ***************************
+            rand = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                treeRed.Insert(rand.Next(0, 1000));
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                treeGreen.Insert(i*i);
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                treeBlue.Insert(rand.Next(0, i));
+            }
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// game-specific content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			// TODO: Unload any non ContentManager content here
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			// After you have the rest of the assignment working:
+			//  What happens if you insert a new piece of 
+			//  data into the trees each frame?
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+
+            // Draw the trees
+            treeRed.Draw(new Vector2(150, 400));
+            treeGreen.Draw(new Vector2(300, 300));
+            treeBlue.Draw(new Vector2(600, 400));
+
+            base.Draw(gameTime);
+		}
+	}
+}
---
+++ DynamicTree/DynamicTreeStarterCodeV2/DynamicTreeStarterCode/Tree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DynamicTreeStarterCode
+{
+	/// <summary>
+	/// Represents a tree-centric data structure
+	/// that can have data dynamically inserted, 
+	/// and can be drawn as a literal "tree" on the screen
+	/// </summary>
+	class Tree : DrawableTree
+	{
+		// Already has an inherited root node field called "root"
+
+		/// <summary>
+		/// Creates a tree that can be drawn
+		/// </summary>
+		/// <param name="sb">The sprite batch used to draw</param>
+		/// <param name="treeColor">The color of this tree</param>
+		public Tree(SpriteBatch sb, Color treeColor)
+			: base(sb, treeColor)
+		{ }
+
+		/// <summary>
+		/// Public facing Insert method
+		/// </summary>
+		/// <param name="data">The data to insert</param>
+		public void Insert(int data)
+		{
+			// *** Fill in this method ****************************************
+			if(root != null)
+            {
+                Insert(data, root);
+            }
+            else
+            {
+                TreeNode node = new TreeNode(data);
+                root = node;
+            }
+
+		}
+
+		/// <summary>
+		/// Private recursive insert method
+		/// </summary>
+		/// <param name="data">The data to insert</param>
+		/// <param name="node">The node to attempt to insert into</param>
+		private void Insert(int data, TreeNode node)
+		{
+			// *** Fill in this method ****************************************
+			if(data < node.Data && node.Left != null)
+            {
+                Insert(data, node.Left);
+            }
+            else if(data < node.Data && node.Left == null)
+            {
+                node.Left = new TreeNode(data);
+            }
+            else if(data >= node.Data && node.Right != null)
+            {
+                Insert(data, node.Right);
+            }
+            else if(data >= node.Data && node.Right == null)
+            {
+                node.Right = new TreeNode(data);
+            }
+		}
+	}
+}
